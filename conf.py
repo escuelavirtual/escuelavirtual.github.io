f3aa9067ff14b1970f979919512e3d2f3e9d5b4a
Alejandro <snackbarchat@gmail.com>
2020-08-06 03:27:53 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -95,7 +95,7 @@ html_additional_pages = {
 }
 
 # Output an OpenSearch description file.
-html_use_opensearch = 'https://docs.python.org/' + version
+html_use_opensearch = 'https://docs.python.org/'
 
 # Additional static files.
 html_static_path = ['tools/static']
