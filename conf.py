f3e3ad71523b2a99a5a5e53439d0a3d29a6471cb
Alejandro <snackbarchat@gmail.com>
2020-08-08 02:18:38 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -64,6 +64,11 @@ html_theme_options = {
 }
 html_context = {}
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
 if not 'READTHEDOCS' in os.environ:
     html_static_path = ['_static/']
     html_js_files = ['debug.js']
